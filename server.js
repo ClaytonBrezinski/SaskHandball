1a63390a8a7a21f7d16cf2e34c7fc34b59f92d89
Unknown <clayton.brez@gmail.com>
2018-08-01 13:01:57 -0600

    allow express to server all files in production

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("testing");
-});
+if (process.env.NODE_ENV === 'production') {
+	// have express serve production assets that exist within the client build file
+	app.use(express.static('client/build'));
 
-const PORT = process.env.PORT || 5000; 
-app.listen(PORT); 
+	// have expess serve up the index.html file if it doesn't recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT);
