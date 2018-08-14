1799159ea75d49ea17271a0acec97294a1e6c230
Unknown <clayton.brez@gmail.com>
2018-08-14 13:36:38 -0600

    add blurred background

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,20 @@ import Payments from './Components/Payments';
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
+			<React.Fragment>
+				<div className="background-image" />
+				<div className="App">
 					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
-				<p className="App-intro">
-					To get started, edit <code>src/App.js</code> and save to
-					reload.
-				</p>
-				<Payments />
-			</div>
+					<header className="App-header">
+						<h1 className="App-title">Welcome to React</h1>
+					</header>
+					<p className="App-intro">
+						To get started, edit <code>src/App.js</code> and save to
+						reload.
+					</p>
+					<Payments />
+				</div>
+			</React.Fragment>
 		);
 	}
 }
