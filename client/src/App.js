9fe8251b288423248bd68501ee179cf8450e9a98
Unknown <clayton.brez@gmail.com>
2018-08-27 18:49:57 -0600

    add grid and structure to frontend panel

--- client/src/App.js
+++ client/src/App.js
@@ -3,24 +3,38 @@ import logo from './logo.svg';
 import './App.css';
 import Payments from './Components/Payments';
 import LeagueSelect from './Components/LeagueSelect';
+import { Grid } from '@material-ui/core';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = {
+	layout: {
+		marginTop: '10%',
+		width: 'auto',
+		display: 'block', // Fix IE11 issue.
+		padding: '10px',
+	},
+};
 
 class App extends Component {
 	render() {
 		return (
 			<React.Fragment>
 				<div className="background-image" />
-
-				<div className="App">
-					<img src={logo} className="App-logo" alt="logo" />
-					<header className="App-header">
-						<h1 className="App-title">Welcome to React</h1>
-					</header>
-					<p className="App-intro">
-						To get started, edit <code>src/App.js</code> and save to
-						reload.
-					</p>
-					<LeagueSelect />
-					<Payments />
+				<div style={styles.layout}>
+					<Grid
+						container
+						spacing={24}
+						direction="column"
+						justify="center"
+						alignItems="center"
+					>
+						<Grid item xs={24} L={24}>
+							<LeagueSelect />
+						</Grid>
+						<Grid item xs={24} L={24}>
+							<Payments />
+						</Grid>
+					</Grid>
 				</div>
 			</React.Fragment>
 		);
