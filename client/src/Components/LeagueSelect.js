17c1901a5f28d33dfb48f8cfdb767b2b0226b1d2
Unknown <clayton.brez@gmail.com>
2018-08-27 18:58:13 -0600

    add card content for frontend panel

--- client/src/Components/LeagueSelect.js
+++ client/src/Components/LeagueSelect.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Dropdown, TextField } from './leagueSelect/index';
 import request from 'request';
+import { Card, CardContent, Grid } from '@material-ui/core';
 
 export default class LeagueSelect extends Component {
 	state = {
@@ -13,11 +14,22 @@ export default class LeagueSelect extends Component {
 	}
 	render() {
 		return (
-			<React.Fragment>
-				<Dropdown label="testLabel" options={['a', 'b']} />
-				<Dropdown label="testLabel2" options />
-				<TextField name="test1" />
-			</React.Fragment>
+			<Card raised="true" style={{ padding: '15px' }}>
+				<CardContent>
+					<Grid
+						container
+						spacing={24}
+						direction="column"
+						justify="center"
+						alignItems="center"
+					>
+						<Dropdown label="League" options={['a', 'b']} />
+						<Dropdown label="Team" options />
+						<TextField name="FirstName" />
+						<TextField name="LastName" />
+					</Grid>
+				</CardContent>
+			</Card>
 		);
 	}
 }
