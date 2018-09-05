a3d3b2161773dddc2eaf2700264051335ba5cb4d
OriginalMidas <clayton.brez@gmail.com>
2018-09-05 12:52:19 -0600

    add enzyme to json for snapshot testing

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,5 +1,5 @@
-const Enzyme = require('enzyme');
-const EnzymeAdapter = require('enzyme-adapter-react-16');
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-// Setup enzyme's react adapter
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
